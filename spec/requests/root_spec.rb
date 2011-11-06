cc01688d5d7e395998c84f882e4a3a96a876edfb
Ryan Moran <ryan.moran@gmail.com>
2011-11-05 18:04:57 -0700

    root/home page projects link to their show page

--- spec/requests/root_spec.rb
+++ spec/requests/root_spec.rb
@@ -3,16 +3,16 @@ require 'spec_helper'
 describe 'root' do
 
   before(:each) do
-    Project.create(:name => 'Huddle')
-    Project.create(:name => 'Polaris')
+    @huddle  = Project.create(:name => 'Huddle')
+    @polaris = Project.create(:name => 'Polaris')
     visit root_path
   end
 
-  it 'displays a list of deployed projects' do
+  it 'displays a list of deployed projects, with links to the show page for that project' do
     page.should have_content('Projects')
     within_table('projects') do
-      page.should have_content('Huddle')
-      page.should have_content('Polaris')
+      page.should have_xpath("./tr[1]/td[1]/a[@href='/projects/#{@huddle.id}' and contains(text(), 'Huddle')]")
+      page.should have_xpath("./tr[2]/td[1]/a[@href='/projects/#{@polaris.id}' and contains(text(), 'Polaris')]")
     end
   end
 

467963a4cd1b0c1ae2158cfc75013ced7f748d3d
Ryan Moran <ryan.moran@gmail.com>
2011-11-19 18:03:03 -0800

    deployments nested under a project in their routes will only show deployments for that project

--- app/controllers/deployments_controller.rb
+++ app/controllers/deployments_controller.rb
@@ -1,7 +1,11 @@
 class DeploymentsController < ApplicationController
 
+  before_filter :load_project, :only => :index
+
   def index
-    @deployments = DeploymentDecorator.decorate(Deployment.includes(:project).all)
+    @deployments = Deployment.includes(:project)
+    @deployments = @deployments.where(:project_id => @project.id) if @project
+    @deployments = DeploymentDecorator.decorate(@deployments)
   end
 
   def show
@@ -11,6 +15,12 @@ class DeploymentsController < ApplicationController
   def create
     @deployment = Deployment.create!(params[:deployment])
     render :nothing => true
+  end
+
+  protected
+
+  def load_project
+    @project = Project.find(params[:project_id]) if params[:project_id]
   end
 
 end
--- spec/requests/deployments/index_spec.rb
+++ spec/requests/deployments/index_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 describe 'deployments#index' do
 
   before(:each) do
-    @huddle = create(:project, :name => 'Huddle')
+    @huddle  = create(:project, :name => 'Huddle')
+    @polaris = create(:project, :name => 'Polaris')
     @deployment_1 = create(:deployment,
       :description => 'Deploying new versions of the Desmos gem.',
       :deployed_at => DateTime.parse('2011-01-01T00:00:00-08:00')
@@ -17,52 +18,78 @@ describe 'deployments#index' do
       :deployed_at => DateTime.parse('2011-01-02T01:00:00-08:00')
     )
 
-    @huddle.deployments << @deployment_1 << @deployment_2 << @deployment_3
+    @huddle.deployments << @deployment_1 << @deployment_2
     @huddle.save
+
+    @polaris.deployments << @deployment_3
+    @polaris.save
 
     @deployment_1 = @deployment_1.decorator
     @deployment_2 = @deployment_2.decorator
     @deployment_3 = @deployment_3.decorator
-
-    visit deployments_path
   end
 
-  it 'displays a list of deployments grouped by day in reverse chronological order' do
-    page.should have_content('1/1')
-    within_table('deployments-2011-01-01') do
-      page.should have_xpath("./tr[@id='deployment-#{@deployment_1.id}']")
+  context 'when viewing deployments for all projects' do
+
+    before(:each) do
+      visit deployments_path
     end
-    page.should have_content('1/2')
-    within_table('deployments-2011-01-02') do
-      page.should have_xpath("./tr[1][@id='deployment-#{@deployment_2.id}']")
-      page.should have_xpath("./tr[2][@id='deployment-#{@deployment_3.id}']")
+
+    it 'displays a list of deployments grouped by day in reverse chronological order' do
+      page.should have_content('1/1')
+      within_table('deployments-2011-01-01') do
+        page.should have_xpath("./tr[@id='deployment-#{@deployment_1.id}']")
+      end
+      page.should have_content('1/2')
+      within_table('deployments-2011-01-02') do
+        page.should have_xpath("./tr[1][@id='deployment-#{@deployment_2.id}']")
+        page.should have_xpath("./tr[2][@id='deployment-#{@deployment_3.id}']")
+      end
     end
+
+    context 'for a given deployment' do
+
+      it "displays the deployment's timestamp" do
+        within(:xpath, "//tr[@id='#{@deployment_1.to_view_id}']/td[1]") do
+          page.should have_xpath("./a[@href='/deployments/#{@deployment_1.id}' and contains(text(), '12:00AM')]")
+        end
+      end
+
+      it "displays the deployed project's name" do
+        within(:xpath, "//tr[@id='#{@deployment_1.to_view_id}']/td[2]") do
+          page.should have_xpath("./a[@href='/projects/#{@huddle.id}' and contains(text(), 'Huddle')]")
+        end
+      end
+
+      it "displays the deployer's name" do
+        within(:xpath, "//tr[@id='#{@deployment_1.to_view_id}']/td[3]") do
+          page.should have_content('ryan')
+        end
+      end
+
+      it "displays an abbreviated version of the deployment's description" do
+        within(:xpath, "//tr[@id='#{@deployment_1.to_view_id}']/td[4]") do
+          page.should have_content('Deploying new versions of...')
+          page.should have_xpath("./a[@href='/deployments/#{@deployment_1.id}' and contains(text(), '...')]")
+        end
+      end
+
+    end
+
   end
 
-  context 'for a given deployment' do
+  context 'when viewing deployments for a specific project' do
 
-    it "displays the deployment's timestamp" do
-      within(:xpath, "//tr[@id='#{@deployment_1.to_view_id}']/td[1]") do
-        page.should have_xpath("./a[@href='/deployments/#{@deployment_1.id}' and contains(text(), '12:00AM')]")
-      end
+    before(:each) do
+      visit project_deployments_path(@polaris)
     end
 
-    it "displays the deployed project's name" do
-      within(:xpath, "//tr[@id='#{@deployment_1.to_view_id}']/td[2]") do
-        page.should have_xpath("./a[@href='/projects/#{@huddle.id}' and contains(text(), 'Huddle')]")
-      end
-    end
-
-    it "displays the deployer's name" do
-      within(:xpath, "//tr[@id='#{@deployment_1.to_view_id}']/td[3]") do
-        page.should have_content('ryan')
-      end
-    end
-
-    it "displays an abbreviated version of the deployment's description" do
-      within(:xpath, "//tr[@id='#{@deployment_1.to_view_id}']/td[4]") do
-        page.should have_content('Deploying new versions of...')
-        page.should have_xpath("./a[@href='/deployments/#{@deployment_1.id}' and contains(text(), '...')]")
+    it 'displays the list of deployments for this project only' do
+      page.should_not have_content('1/1')
+      page.should have_content('1/2')
+      within_table('deployments-2011-01-02') do
+        page.should_not have_xpath("./tr[1][@id='deployment-#{@deployment_2.id}']")
+        page.should have_xpath("./tr[1][@id='deployment-#{@deployment_3.id}']")
       end
     end
 
